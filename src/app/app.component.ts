9c19d56da079332c694cd9af552f264386544621
biyori-t <takuro.toji@gmail.com>
2022-05-19 00:57:49 +0900

    #1 Todo 追加機能を仮実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tutorial-todo';
+  todoes: string[] = [];
+
+  constructor() {}
+
+  addTodo(todo: HTMLInputElement) {
+    if (todo.value) {
+      this.todoes.push(todo.value);
+      todo.value = '';
+    }
+  }
 }
