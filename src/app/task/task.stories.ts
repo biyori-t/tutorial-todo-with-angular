9d9ec8b39ea11b4bab27975cf3517b9b5562f64a
biyori-t <takuro.toji@gmail.com>
2022-05-27 22:56:36 +0900

    #3 ストーリーの仮作成

---
+++ src/app/task/task.stories.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { moduleMetadata } from '@storybook/angular';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { action } from '@storybook/addon-actions';
+import { TaskComponent } from './task.component';
+
+export default {
+  component: TaskComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [TaskComponent],
+      imports: [CommonModule],
+    }),
+  ],
+  excludeStories: /.*Data$/,
+  title: 'Todo',
+} as Meta;
+
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+const Template: Story<TaskComponent> = (args) => ({
+  props: {
+    ...args,
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+    updatedAt: new Date(2022, 5, 26, 21, 47),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
