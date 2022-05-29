33bf6f2298bab574218daa0aaa986becbee36c2f
biyori-t <takuro.toji@gmail.com>
2022-05-29 16:02:22 +0900

    #3 Todo をCSF3の書き方に変更

--- src/app/todoes/todo/todo.stories.ts
+++ src/app/todoes/todo/todo.stories.ts
@@ -1,27 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { moduleMetadata } from '@storybook/angular';
-import { Story, Meta } from '@storybook/angular/types-6-0';
+import { Meta, StoryObj } from '@storybook/angular';
 import { TodoComponent } from './todo.component';
 
 export default {
   component: TodoComponent,
-  decorators: [
-    moduleMetadata({
-      declarations: [TodoComponent],
-      imports: [CommonModule],
-    }),
-  ],
   excludeStories: /.*Data$/,
-  title: 'Todo',
-} as Meta;
+} as Meta<typeof TodoComponent>;
 
-const Template: Story<TodoComponent> = (args) => ({
-  props: {
-    ...args,
+export const Default: StoryObj = {
+  render: (args) => ({
+    template: `<app-todo [todo]="todo"></app-todo>`,
+    props: args,
+  }),
+  args: {
+    todo: 'Todo をここに表示する',
   },
-});
-
-export const Default = Template.bind({});
-Default.args = {
-  todo: 'Todo をここに表示する',
 };
