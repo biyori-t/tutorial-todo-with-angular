d0f533c9e6fdca0f624896341c9359ac8a62631c
biyori-t <takuro.toji@gmail.com>
2022-06-01 21:30:04 +0900

    #2 Todo 削除のストーリーを追加

--- src/app/todoes/todo/todo.stories.ts
+++ src/app/todoes/todo/todo.stories.ts
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from '@storybook/angular';
 import { TodoComponent } from './todo.component';
 
+import { screen, fireEvent } from '@storybook/testing-library';
+
 export default {
   component: TodoComponent,
 } as Meta;
@@ -12,5 +14,19 @@ export const Default: StoryObj<TodoComponent> = {
   }),
   args: {
     todo: 'Todo をここに表示する',
+  },
+};
+
+export const Delete: StoryObj<TodoComponent> = {
+  render: (args) => ({
+    template: `<app-todo [todo]="todo"></app-todo>`,
+    props: args,
+  }),
+  args: {
+    todo: 'Todo',
+  },
+  play: async () => {
+    const deleteButton = screen.getByRole('button');
+    fireEvent.click(deleteButton);
   },
 };
